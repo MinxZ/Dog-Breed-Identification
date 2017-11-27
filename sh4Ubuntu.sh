6cb6db6ead618f327a12784241a901a8c38b94fa
MinxZ <z@Zs-MacBook-Pro.local>
2017-11-27 11:37:24 +0900

    Add shell for ubuntu to run

---
+++ sh4Ubuntu.sh
@@ -0,0 +1,4 @@
+wget 'https://www.kaggle.com/c/dog-breed-identification/download/labels.csv.zip'
+wget 'https://www.kaggle.com/c/dog-breed-identification/download/sample_submission.csv.zip'
+wget 'https://www.kaggle.com/c/dog-breed-identification/download/train.zip'
+wget 'https://www.kaggle.com/c/dog-breed-identification/download/test.zip'
