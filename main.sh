097c6b71a83fbc237d6b36eafe8519c1f2b16752
MinxZ <z@Zs-MacBook-Pro.local>
2017-11-28 11:01:32 +0900

    add something

--- main.sh
+++ main.sh
@@ -2,12 +2,13 @@ cd ..
 
 mkdir dog_breed_datasets
 cd dog_breed_datasets
-wget 'https://www.kaggle.com/c/dog-breed-identification/download/labels.csv.zip'
-wget 'https://www.kaggle.com/c/dog-breed-identification/download/sample_submission.csv.zip'
-wget 'https://www.kaggle.com/c/dog-breed-identification/download/train.zip'
-wget 'https://www.kaggle.com/c/dog-breed-identification/download/test.zip'
+wget --load-cookies cookies.txt \
+        https://www.kaggle.com/c/dog-breed-identification/download/labels.csv.zip \
+        https://www.kaggle.com/c/dog-breed-identification/download/sample_submission.csv.zip \
+        https://www.kaggle.com/c/dog-breed-identification/download/train.zip \
+        https://www.kaggle.com/c/dog-breed-identification/download/test.zip
 
-unzip *.zip
+unzip '*.zip'
 rm *.zip
 
 cd ../Dog-Breed-Identification
