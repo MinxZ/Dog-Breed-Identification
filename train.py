8df7ab7941621d13be9582a7ae9e14d82e8fbd3e
MinxZ <z@Zs-MacBook-Pro.local>
2017-12-13 23:14:09 +0900

    Add reduce_lr to train.py.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import pandas as pd
 from keras import backend as K
 from keras.applications import *
 from keras.applications.inception_v3 import preprocess_input
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.callbacks import *
 from keras.layers import *
 from keras.models import *
 from keras.optimizers import *
@@ -23,43 +23,45 @@ from keras.utils.vis_utils import model_to_dot
 from tqdm import tqdm
 
 
-def run(model_name, lr, optimizer, epoch, patience, batch_size):
+def run(model_name, lr, optimizer, epoch, patience, batch_size, test=None):
     # Loading Datasets
+    def load_data():
 
-    df = pd.read_csv('../dog_breed_datasets/labels.csv')
-    df.head()
+        df = pd.read_csv('../dog_breed_datasets/labels.csv')
+        df.head()
 
-    n = len(df)
-    breed = set(df['breed'])
-    n_class = len(breed)
-    class_to_num = dict(zip(breed, range(n_class)))
-    num_to_class = dict(zip(range(n_class), breed))
+        n = len(df)
+        breed = set(df['breed'])
+        n_class = len(breed)
+        class_to_num = dict(zip(breed, range(n_class)))
+        num_to_class = dict(zip(range(n_class), breed))
 
-    width = 299
-    X = np.zeros((n, width, width, 3), dtype=np.uint8)
-    y = np.zeros((n, n_class), dtype=np.uint8)
-    # Loading Datasets
-    print('\n\n Loading Datasets. \n')
-    for i in tqdm(range(n)):
-        X[i] = cv2.resize(
-            cv2.imread('../dog_breed_datasets/train/%s.jpg' % df['id'][i]),
-            (width, width))
-        y[i][class_to_num[df['breed'][i]]] = 1
+        width = 299
+        X = np.zeros((n, width, width, 3), dtype=np.uint8)
+        y = np.zeros((n, n_class), dtype=np.uint8)
+        # Loading Datasets
+        print('\n\n Loading Datasets. \n')
+        for i in tqdm(range(n)):
+            X[i] = cv2.resize(
+                cv2.imread('../dog_breed_datasets/train/%s.jpg' % df['id'][i]),
+                (width, width))
+            y[i][class_to_num[df['breed'][i]]] = 1
 
-    dvi = int(X.shape[0] * 0.9)
-    x_train = X[:dvi, :, :, :]
-    y_train = y[:dvi, :]
-    x_val = X[dvi:, :, :, :]
-    y_val = y[dvi:, :]
+        dvi = int(X.shape[0] * 0.9)
+        x_train = X[:dvi, :, :, :]
+        y_train = y[:dvi, :]
+        x_val = X[dvi:, :, :, :]
+        y_val = y[dvi:, :]
+        return x_train, y_train, x_val, y_val
+
+    x_train, y_train, x_val, y_val = load_data()
 
     # Compute the bottleneck feature
-
     def get_features(MODEL, data=X):
         cnn_model = MODEL(
             include_top=False,
             input_shape=(width, width, 3),
             weights='imagenet')
-
         inputs = Input((width, width, 3))
         x = inputs
         x = Lambda(preprocess_input, name='preprocessing')(x)
@@ -77,21 +79,22 @@ def run(model_name, lr, optimizer, epoch, patience, batch_size):
                   epoch,
                   patience,
                   batch_size,
-                  X=X):
+                  X=X,
+                  test=None):
         # Fine-tune the model
         print("\n\n Fine tune " + model_name + " : \n")
 
+        from random_eraser import get_random_eraser
         datagen = ImageDataGenerator(
-            preprocessing_function=preprocess_input,
-            shear_range=0.2,
-            zoom_range=0.2,
+            reprocessing_function=get_random_eraser(pixel_level=True),
             horizontal_flip=True,
-            rotation_range=20,
-            width_shift_range=0.2,
-            height_shift_range=0.2)
+            shear_range=0.1,
+            zoom_range=0.1,
+            rotation_range=10,
+            width_shift_range=0.1,
+            height_shift_range=0.1)
 
-        val_datagen = ImageDataGenerator(
-            preprocessing_function=preprocess_input)
+        val_datagen = ImageDataGenerator()
 
         inputs = Input((width, width, 3))
         x = inputs
@@ -105,6 +108,7 @@ def run(model_name, lr, optimizer, epoch, patience, batch_size):
         x = Dense(n_class, activation='softmax', name='predictions')(x)
         model = Model(inputs=inputs, outputs=x)
 
+        # Loading weights
         try:
             model.load_weights(model_name + '.h5')
             print('Load ' + model_name + '.h5 successfully.')
@@ -138,10 +142,9 @@ def run(model_name, lr, optimizer, epoch, patience, batch_size):
                 model_fc.save('fc_' + model_name + '.h5', 'w')
 
         print("\n " + "Optimizer=" + optimizer + " lr=" + str(lr) + " \n")
-        optimizer
-        if optimizer == "Nadam":
+        if optimizer == "Adam":
             model.compile(
-                optimizer=Nadam(lr=lr),
+                optimizer='adam',
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
         elif optimizer == "SGD":
@@ -149,37 +152,46 @@ def run(model_name, lr, optimizer, epoch, patience, batch_size):
                 loss='categorical_crossentropy',
                 optimizer=SGD(lr=lr, momentum=0.9, nesterov=True),
                 metrics=['accuracy'])
+        if not test:
+            class LossHistory(keras.callbacks.Callback):
+                def on_train_begin(self, logs={}):
+                    self.losses = []
+                def on_epoch_end(self, batch, logs={}):
+                    self.losses.append((logs.get('loss'), logs.get("val_loss")))
 
-        class LossHistory(keras.callbacks.Callback):
-            def on_train_begin(self, logs={}):
-                self.losses = []
+            history = LossHistory()
 
-            def on_epoch_end(self, batch, logs={}):
-                self.losses.append((logs.get('loss'), logs.get("val_loss")))
+            early_stopping = EarlyStopping(
+                monitor='val_loss', patience=patience, verbose=1, mode='auto')
+            checkpointer = ModelCheckpoint(
+                filepath=model_name + '.h5', verbose=0, save_best_only=True)
+            reduce_lr = ReduceLROnPlateau(factor=0.2, patience=3, verbose=1)
+            model.fit_generator(
+                datagen.flow(x_train, y_train, batch_size=batch_size),
+                steps_per_epoch=len(x_train) / batch_size,
+                validation_data=val_datagen.flow(
+                    x_val, y_val, batch_size=batch_size),
+                validation_steps=len(x_val) / batch_size,
+                epochs=epoch,
+                callbacks=[history, early_stopping, checkpointer, reduce_lr])
+            with open(model_name + ".csv", 'a') as f_handle:
+                np.savetxt(f_handle, history.losses)
+        else:
+            print('Evalute on test set')
+            val_datagen.fit(x_test)
+            score = model.evaluate_generator(
+                val_datagen.flow(x_test, y_test, batch_size=batch_size),
+                len(x_test) / batch_size)
+            print(score)
+            return score
 
-        history = LossHistory()
-
-        early_stopping = EarlyStopping(
-            monitor='val_loss', patience=patience, verbose=1, mode='auto')
-        checkpointer = ModelCheckpoint(
-            filepath=model_name + '.h5', verbose=1, save_best_only=True)
-        h2 = model.fit_generator(
-            datagen.flow(x_train, y_train, batch_size=batch_size),
-            steps_per_epoch=len(x_train) / batch_size,
-            validation_data=val_datagen.flow(
-                x_val, y_val, batch_size=batch_size),
-            validation_steps=len(x_val) / batch_size,
-            epochs=epoch,
-            callbacks=[early_stopping, checkpointer, history])
-        with open(model_name + ".csv", 'a') as f_handle:
-            np.savetxt(f_handle, history.losses)
     list_model = {
         "Xception": Xception,
         "InceptionV3": InceptionV3,
         "InceptionResNetV2": InceptionResNetV2
     }
     fine_tune(list_model[model_name], model_name, optimizer, lr, epoch,
-              patience, batch_size, X)
+              patience, batch_size, X, test)
 
 
 def parse_args():
@@ -189,13 +201,13 @@ def parse_args():
     parser.add_argument(
         "--model", help="Model to use", default="Xception", type=str)
     parser.add_argument(
-        "--lr", help="learning rate", default=0.0001, type=float)
+        "--lr", help="learning rate", default=0.0005, type=float)
     parser.add_argument(
-        "--optimizer", help="optimizer", default="Nadam", type=str)
+        "--optimizer", help="optimizer", default="Adam", type=str)
     parser.add_argument(
-        "--epoch", help="Number of epochs", default=100, type=int)
+        "--epoch", help="Number of epochs", default=1e5, type=int)
     parser.add_argument(
-        "--patience", help="Patience to wait", default=1, type=int)
+        "--patience", help="Patience to wait", default=7, type=int)
     parser.add_argument(
         "--batch_size", help="Batch size", default=16, type=int)
 
@@ -206,4 +218,3 @@ if __name__ == "__main__":
     args = parse_args()
     run(args.model, args.lr, args.optimizer, args.epoch, args.patience,
         args.batch_size)
-
